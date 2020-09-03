7bf1143ca41181b442310e0393ce68d92b21b202
Nathan <11954405+ngsctt@users.noreply.github.com>
2020-09-03 22:37:47 +1000

    Update setup.py

--- library/setup.py
+++ library/setup.py
@@ -11,13 +11,13 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: System :: Hardware']
 
 setup(name='ST7735',
-      version='0.0.4',
+      version='0.0.5',
       description='Library to control an ST7735 168x80 TFT LCD display.',
       long_description=open('README.rst').read() + '\n' + open('CHANGELOG.txt').read(),
       license='MIT',
-      author='Philip Howard',
+      author='Philip Howard, ngsctt',
       author_email='phil@pimoroni.com',
       classifiers=classifiers,
-      url='https://github.com/pimoroni/st7735-160x80-python/',
+      url='https://github.com/ngsctt/st7735-python',
       packages=find_packages(),
       install_requires=['spidev>=3.4'])
